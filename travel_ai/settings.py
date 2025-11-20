cc2724f16932693af73c527abe44d62e911ba7d3
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2025-11-20 15:17:47 +0100

    Set DEBUG to False for production settings

--- travel_ai/settings.py
+++ travel_ai/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15y-#h8bp61hmbbb9_do)4rt0-$ligg794z1jnkp@e^%(td^#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://travel-ai-app-production.up.railway.app"]
