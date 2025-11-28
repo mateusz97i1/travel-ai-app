bb662040a6c6b3edde3b34561cf625fb7d0451da
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2025-11-28 16:23:03 +0100

    v2 common bugs fix

--- travel_app/urls.py
+++ travel_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('findmore',views.find_more_page,name='find_more'),
     path('aboutpage',views.about_page,name='about'),
+    path('privacypolicy',views.privacy_policy,name='privacy_policy'),
     path('contactpage',views.contact_page,name='contact')
 
 ]
--- travel_app/views.py
+++ travel_app/views.py
@@ -102,6 +102,10 @@ def about_page(request):
 
     return render(request, 'about.html')
 
+def privacy_policy(request):
+
+    return render(request, 'privacy_policy.html')
+
 def contact_page(request):
     form=ContactForm
     if request.method=="POST":
